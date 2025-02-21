90ff7ec85bf606ee9a48dc70ccd838d36676b506
Hanief Dafi Satria <satria1002@gmail.com>
2025-02-21 11:19:44 +0700

    [FEATURE] adding instalment type entity

---
+++ src/main/java/com/loanapp/loanapp/constant/EInstalmentType.java
@@ -0,0 +1,9 @@
+package com.loanapp.loanapp.constant;
+
+public enum EInstalmentType {
+    ONE_MONTH,
+    THREE_MONTHS,
+    SIXTH_MONTHS,
+    NINE_MONTHS,
+    TWELVE_MONTHS
+}
---
+++ src/main/java/com/loanapp/loanapp/entity/InstalmentType.java
@@ -0,0 +1,21 @@
+package com.loanapp.loanapp.entity;
+
+import com.loanapp.loanapp.constant.EInstalmentType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "t_instalment_type")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InstalmentType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "instalment_type")
+    private EInstalmentType instalmentType;
+}
